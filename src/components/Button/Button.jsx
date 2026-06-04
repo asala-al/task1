9ad49fd38795938f2c92f41b120e0b18e406214f
asala <alghadbanasala@gmail.com>
2026-06-05 01:11:44 +0200

    feat(Button) : add button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,1 +1,12 @@
 
+import style from './Button.module.css'
+
+function Button() {
+  return (
+    <div>
+      <button className={style.btn}>click me</button>
+    </div>
+  )
+}
+
+export default Button
